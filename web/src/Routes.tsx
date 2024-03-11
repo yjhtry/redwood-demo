21c3556d623c7a63fc495d23d84683a653abd2fd
yjh <860622588@qq.com>
2024-03-11 19:28:59 +0800

    feat: auth

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,26 +7,34 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route, PrivateSet } from '@redwoodjs/router'
 
 import BlogLayout from 'src/layouts/BlogLayout'
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
+import { useAuth } from './auth'
+
 const Routes = () => {
   return (
-    <Router>
+    <Router useAuth={useAuth}>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={BlogLayout}>
         <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
         <Route path="/contact" page={ContactPage} name="contact" />
       </Set>
-      <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
-        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/posts" page={PostPostsPage} name="posts" />
-      </Set>
+      <PrivateSet unauthenticated="home">
+        <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
+          <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
+          <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+          <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
+          <Route path="/admin/posts" page={PostPostsPage} name="posts" />
+        </Set>
+      </PrivateSet>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,28 +1,42 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import { useAuth } from 'src/auth'
+
 type BlogLayoutProps = {
   children?: React.ReactNode
 }
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
+  const { isAuthenticated, logOut, currentUser } = useAuth()
+  console.log(currentUser)
   return (
     <>
       <header className="flex justify-between border-b border-b-gray-300 py-2 pl-3 pr-8">
         <h1 className="text-base font-bold">
           <Link to={routes.home()}>Redwood Blog</Link>
         </h1>
-        <nav>
+        <nav className="flex gap-6">
           <ul className="flex gap-4">
-            <li>
+            <li className="hover:text-blue-300">
               <Link to={routes.home()}>Home</Link>
             </li>
-            <li>
+            <li className="hover:text-blue-300">
               <Link to={routes.about()}>About</Link>
             </li>
-            <li>
+            <li className="hover:text-blue-300">
               <Link to={routes.contact()}>Contact</Link>
             </li>
           </ul>
+          {isAuthenticated ? (
+            <div>
+              <span>{currentUser.email as string}</span>
+              <button className="ml-2" type="button" onClick={logOut}>
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link to={routes.login()}>Login</Link>
+          )}
         </nav>
       </header>
       <main>{children}</main>
